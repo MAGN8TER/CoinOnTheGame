e60240d1e1615fb47194787547185c88f4044517
MAGN8TER <magnusbehnke07@gmail.com>
2026-01-08 00:33:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.onload = function() {
 };
 
 function loadNBAGames() {
-    // Fetch from your local JSON file (no API key needed here!)
+    // Fetch from your local JSON file
     fetch("./nba_data.json")
         .then(res => {
             if (!res.ok) throw new Error("Data not found");
@@ -14,18 +14,22 @@ function loadNBAGames() {
             const container = document.querySelector(".gamesFormat");
             if (!container) return;
 
-            container.innerHTML = ""; // Clear placeholders
+            container.innerHTML = ""; // Clear the empty container
 
             data.games.forEach(game => {
+                // 1. Create the card container
                 const card = document.createElement("div");
                 card.className = "gameCardUI";
                 
-                // Content mapped to your CSS grid
+                // 2. Insert 3 divs to fill the 3 grid columns defined in style.css
+                // We add justify-self: center to mimic your 'sport' placeholder alignment
                 card.innerHTML = `
-                    <div class="team-name">${game.visitor}</div>
-                    <div class="game-time">${game.time}</div>
-                    <div class="team-name">${game.home}</div>
+                    <div style="justify-self: center;">${game.visitor}</div>
+                    <div style="justify-self: center;">${game.time}</div>
+                    <div style="justify-self: center;">${game.home}</div>
                 `;
+
+                // 3. Add the finished card to the page
                 container.appendChild(card);
             });
         })
